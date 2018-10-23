457514acd2dece5bc3aaa302f2ed5767c358945b
DavidMongui <44002907+DavidMongui@users.noreply.github.com>
2018-10-23 13:51:10 -0500

    actualizacion de register

--- angular/src/app/models/user.ts
+++ angular/src/app/models/user.ts
@@ -1,18 +1,25 @@
 export class User {
 
-    constructor(name = '', cc = '', mail = '', password = '', confirmPassword = '', addres = '') {
-        this.name = name;
-        this.cc = cc;
-        this.mail = mail;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
-        this.addres = addres;
+    constructor(nombre = '', id = '', correo = '', telefono = '', 
+    contrasena = '', codigo = '', id_pais = '', id_rol = '',numCuenta = '') {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.contrasena = contrasena;
+        this.codigo = codigo;
+        this.id_pais = id_pais;
+        this.id_rol = id_rol;
+        this.numCuenta = numCuenta;
     }
-    name: string;
-    cc : string;
-    mail : string;
-    password : string;
-    confirmPassword : string;
-    addres : string;    
+    nombre: string;
+    id : string;
+    correo : string;
+    telefono : string;
+    contrasena : string;
+    codigo : string;
+    id_pais : string;
+    id_rol : string;
+    numCuenta : string;    
 
 }
