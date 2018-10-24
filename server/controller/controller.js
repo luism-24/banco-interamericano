23f2fb3b8597282d461a58972cb6012c144a34fb
Andres <personalQuinto@gmail.com>
2018-10-24 11:33:46 -0500

    estilo front

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -12,13 +12,23 @@ const conec = require('../conect/conection');
 
 controll.registre = (req, res) => {
 
+    /*var nombre = req.body.nombre;
+    var correo = req.body.correo;
+    var telefono = req.body.telefono;
+    var contraseña = req.body.contraseña;
+    var idPais = req.body.idPais;
+    var idRol = req.body.idRol;
+    var numCuenta = req.body.numCuenta;*/
+
+    console.log(req.body);
+
     res.send('Registro de usuarios');
     /////////////////Se toma el numero ingresado si se registra correctamente//////////////
     var numero = '3187934956';
-    var mensaje = '4743.';
+    var codigo = aleatorio(1000, 9000);
 
     nexmo.message.sendSms(
-        '573187934956', '57' + numero, 'Codigo de registro: ' + mensaje,
+        '573187934956', '57' + numero, 'Codigo de registro: ' + codigo,
         (err, responseData) => {
             if (err) {
                 console.log(err);
@@ -54,5 +64,9 @@ controll.transfer = (req, res) => {
 
 }
 
+function aleatorio(inferior,superior){ 
+    var resAleatorio = Math.floor((Math.random() * (superior - inferior + 1)) + inferior);
+   return resAleatorio;
+}
 
 module.exports = controll;
